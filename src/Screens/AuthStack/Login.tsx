0e3afa19b4f38e9c1f63326285e94bb86662b604
lrtestdemo <lr.testdemo@gmail.com>
2023-09-27 17:09:32 +0530

    [Update] Navigation set-up added

---
+++ src/Screens/AuthStack/Login.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#FFFFFF'
+  }
+});
+
+export default Login
